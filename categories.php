b7476e346d20c03e2b0f0fcdd8f796e67fac9445
Lena <65555763+SashaNastyaLena@users.noreply.github.com>
2020-05-20 22:54:13 +0300

    button -> link

--- categories.php
+++ categories.php
@@ -49,19 +49,19 @@
                             <img id="img" src="flower_catalog/Heartfelt.jpg" alt="Tulips">
                             <h3 id="name">Heartfelt</h3>
                             <p id="price">69.99 $</p>
-                            <button onclick="addToOrder()" class="order_btn">ORDER</button>
+                            <a class="order_btn" href="order.php?order=1">ORDER</a>
                         </div>
                         <div class="category_box">
                             <img id="img" src="flower_catalog/All_the_Rage.jpg" alt="Tulips">
                             <h3 id="name">All the Rage"</h3>
                             <p id="price">45.00 $</p>
-                            <button onclick="addToOrder()" class="order_btn">ORDER</button>
+                            <a class="order_btn" href="order.php?order=2">ORDER</a>
                         </div>
                         <div class="category_box">
                             <img id="img" src="flower_catalog/Tiger.jpg" alt="Tulips">
                             <h3 id="name">Tiger</h3>
                             <p id="price">54.50 $</p>  
-                            <button onclick="addToOrder()" class="order_btn">ORDER</button>
+                            <a class="order_btn" href="order.php?order=3">ORDER</a>
                         </div>
                     </div>
                  </div>
@@ -75,19 +75,19 @@
                             <img id="img" src="flower_catalog/Double_the_Buttercream.jpg" alt="Wedding bouquets">
                             <h3 id="name">Double the Buttercream</h3>
                             <p id="price">79.00 $</p>
-                            <button onclick="addToOrder()" class="order_btn">ORDER</button>
+                            <a class="order_btn" href="order.php?order=15">ORDER</a>
                         </div>
                         <div class="category_box">
                             <img id="img" src="flower_catalog/Double_the_Unicorn.jpg" alt="Wedding bouquets">
                             <h3 id="name">Double the Unicorn</h3>
                             <p id="price">85.99 $</p>
-                            <button onclick="addToOrder()" class="order_btn">ORDER</button>
+                            <a class="order_btn" href="order.php?order=16">ORDER</a>
                         </div>
                         <div class="category_box">
                             <img id="img" src="flower_catalog/Muse.jpg" alt="Wedding bouquets">
                             <h3 id="name">Muse</h3>
                             <p id="price">150.00 $</p>  
-                            <button onclick="addToOrder()" class="order_btn">ORDER</button>
+                            <a class="order_btn" href="order.php?order=17">ORDER</a>
                         </div>
                     </div>
                  </div>
@@ -101,19 +101,19 @@
                         <img id="img" src="flower_catalog/Buttercream.jpg" alt="Roses">
                             <h3 id="name">Buttercream</h3>
                             <p id="price">65.00 $</p>
-                            <button onclick="addToOrder()" class="order_btn">ORDER</button>
+                            <a class="order_btn" href="order.php?order=7">ORDER</a>
                         </div>
                         <div class="category_box">
                         <img id="img" src="flower_catalog/Santorini.jpg" alt="Roses">
                             <h3 id="name">Santorini</h3>
                             <p id="price">52.00 $</p>
-                            <button onclick="addToOrder()" class="order_btn">ORDER</button>
+                            <a class="order_btn" href="order.php?order=8">ORDER</a>
                         </div>
                         <div class="category_box">
                         <img id="img" src="flower_catalog/Nonna.jpg" alt="Roses">
                             <h3 id="name">Nonna</h3>
                             <p id="price">70.85 $</p>
-                            <button onclick="addToOrder()" class="order_btn">ORDER</button>
+                            <a class="order_btn" href="order.php?order=9">ORDER</a>
                         </div>
                     </div>
                  </div>
@@ -128,19 +128,19 @@
                         <img id="img" src="flower_catalog/Luna.jpg" alt="Flowers">
                             <h3 id="name">Luna</h3>
                             <p id="price">65.70 $</p>
-                            <button onclick="addToOrder()" class="order_btn">ORDER</button>
+                            <a class="order_btn" href="order.php?order=24">ORDER</a>
                         </div>
                         <div class="category_box">
                         <img id="img" src="flower_catalog/Juliet.jpg" alt="Flowers">
                             <h3 id="name">Juliet</h3>
                             <p id="price">64.99 $</p>
-                            <button onclick="addToOrder()" class="order_btn">ORDER</button>
+                            <a class="order_btn" href="order.php?order=25">ORDER</a>
                         </div>
                         <div class="category_box">
                         <img id="img" src="flower_catalog/Regatta.jpg" alt="Flowers">
                             <h3 id="name">Regatta</h3>
                             <p id="price">130.00 $</p>
-                            <button onclick="addToOrder()" class="order_btn">ORDER</button>
+                            <a class="order_btn" href="order.php?order=26">ORDER</a>
                         </div>
                     </div>
                  </div>
@@ -155,19 +155,19 @@
                         <img id="img" src="flower_catalog/Red_and_Blush.jpg" alt="Flowers box">
                             <h3 id="name">Red and Blush</h3>
                             <p id="price">40.00 $</p>
-                            <button onclick="addToOrder()" class="order_btn">ORDER</button>
+                            <a class="order_btn" href="order.php?order=31">ORDER</a>
                         </div>
                         <div class="category_box">
                         <img id="img" src="flower_catalog/Blooming_Song.jpg" alt="Flowers box">
                             <h3 id="name">Blooming Song</h3>
                             <p id="price">64.99 $</p>
-                            <button onclick="addToOrder()" class="order_btn">ORDER</button>
+                            <a class="order_btn" href="order.php?order=32">ORDER</a>
                         </div>
                         <div class="category_box">
                         <img id="img" src="flower_catalog/Pinky.jpg" alt="Flowers box">
                             <h3 id="name">Pinky</h3>
                             <p id="price">78.95 $</p>
-                            <button onclick="addToOrder()" class="order_btn">ORDER</button>
+                            <a class="order_btn" href="order.php?order=33">ORDER</a>
                         </div>
                     </div>
                  </div>
